721d293beadae75015c0ddd321bd3d9f44d0df18
Brennan Johnson <brennanjohnson@eduroam-160-44.dyn.carleton.edu>
2018-09-17 17:31:08 -0500

    latest version of books1.py

--- books1.py
+++ books1.py
@@ -6,34 +6,44 @@ def importFile(fileName):
     #returns list of authors and list of books
     inFile = open(fileName, string)
     return inFile
-        
 
-def readFile(fileName):
+
+def readFile(fileName, action):
     authorList = []
     titleList = []
     with open(fileName, newline = '') as bookfile:
         reader = csv.reader(bookfile, delimiter = ',', quotechar = '"')
-        for row in reader:
-            titleList.append(row[0])
-            authorList.append(row[2])
-    return authorList, titleList
+        if action == "books":
+            for row in reader:
+                titleList.append(row[0])
+            #return list of books
+            #add another if for sort-direction
+        return titleList
+        elif action == "authors":
+            for row in reader:
+                authorList.append(row[2])
+            #return list of authors
+            #add another if for sort-direction
+        return authorList
+        else:
+            #return error message?
+            pass
 
 
-def sort(sortDirection, myList):
-    #sorts list based on sortDirection
+
+def sort(sortDirectionBool, myList):
+    #sorts list based on sortDirectionBool
 
 
 def main():
-    filename = sys.argv[0]
-    action = sys.argv[1]
+    fileName = sys.argv[1]
+    action = sys.argv[2]
     if sys.argv[].len() == 3:
-        sortDirection = sys.argv[2]
+        sortDirection = sys.argv[3]
+        sortDirectionBool = False
+        if (sortDirection.lower() == "reverse"):
+            sortDirectionBool = True
+        #sortDirectionBool = true means reverse. if the bool is false, it means
+        #sort forwards
     action = action.lower()
-    if action == "books":
-        #return list of books
-        #add another if for sort-direction
-    elif action == "authors":
-        #return list of authors
-        #add another if for sort-direction
-    else:
-        #return error message?
+    actionList = readFile(fileName, action)
