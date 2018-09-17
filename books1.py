64a2cc0c099ce4d95fbdccd6e41143e576d3135a
Brennan Johnson <brennanjohnson@eduroam-160-44.dyn.carleton.edu>
2018-09-16 20:31:30 -0500

    first bit of skeleton code

---
+++ books1.py
@@ -0,0 +1,31 @@
+import sys
+
+def importFile(filename):
+    #takes in file object, reads csv into 2 lists
+    #returns list of authors and list of books
+
+
+
+
+
+
+def sort(sortDirection, list):
+    #sorts list based on sortDirection
+
+
+
+
+def main():
+    filename = sys.argv[0]
+    action = sys.argv[1]
+    if sys.argv[].len() == 3:
+        sortDirection = sys.argv[2]
+    action = action.lower()
+    if action == "books":
+        #return list of books
+        #add another if for sort-direction
+    elif action == "authors":
+        #return list of authors
+        #add another if for sort-direction
+    else:
+        #return error message?
