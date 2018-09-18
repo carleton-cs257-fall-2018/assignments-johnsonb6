25d270e3ba667cbbdfdcebed83960f54a6dfa005
Alexis Engel <alexisengel99@gmail.com>
2018-09-18 13:15:32 -0500

    added book object

--- books1.py
+++ books1.py
@@ -1,14 +1,32 @@
 import sys
 import csv
 
+class Author:
+    def __init__(self, name, book, publishDate, birthYear, deathYear = 0):
+        self.name = name
+        self.book = [book]
+        self.publishDate = publishDate
+        self.birthYear = birthYear
+        self.deathYear = deathYear
+        self.age = 0
+
+    def getAge(self):
+        if deathYear == 0:
+            age = 2018 - birthYear
+            return age
+        age = deathYear - birthYear
+        return age
+
 def importFile(fileName):
     #takes in file object, reads csv into 2 lists
     #returns list of authors and list of books
     inFile = open(fileName, string)
+    print("import file")
     return inFile
 
 
 def readFile(fileName, action):
+    print("read file")
     authorList = []
     titleList = []
     with open(fileName, newline = '') as bookfile:
@@ -16,14 +34,17 @@ def readFile(fileName, action):
         if action == "books":
             for row in reader:
                 titleList.append(row[0])
+                print(row[0])
             #return list of books
             #add another if for sort-direction
+            print(titleList)
             return titleList
         elif action == "authors":
             for row in reader:
                 authorList.append(row[2])
             #return list of authors
             #add another if for sort-direction
+            print(authorList)
             return authorList
         else:
             #return error message?
@@ -33,13 +54,18 @@ def readFile(fileName, action):
 
 def sort(sortDirectionBool, myList):
     #sorts list based on sortDirectionBool
+    myList.sort()
+    return myList
     pass
 
 
 def main():
+    print("start")
     fileName = sys.argv[1]
+    print(fileName)
     action = sys.argv[2]
-    if sys.argv[].len() == 3:
+    print(action)
+    if sys.argv.len() == 3:
         sortDirection = sys.argv[3]
         sortDirectionBool = False
         if (sortDirection.lower() == "reverse"):
