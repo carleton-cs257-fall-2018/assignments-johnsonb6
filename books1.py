09cd522847dc995947634910897a3c6a052305dd
Alexis Engel <alexisengel99@gmail.com>
2018-09-18 15:07:46 -0500

    a ton of edits

--- books1.py
+++ books1.py
@@ -2,8 +2,9 @@ import sys
 import csv
 
 class Author:
-    def __init__(self, name, book, publishDate, birthYear, deathYear = 0):
-        self.name = name
+    def __init__(self, firstName, lastName, book, publishDate, birthYear, deathYear = 0):
+        self.firstName = firstName
+        self.lastName = lastName
         self.book = [book]
         self.publishDate = publishDate
         self.birthYear = birthYear
@@ -33,30 +34,77 @@ def readFile(fileName, action):
     titleList = []
     with open(fileName, newline = '') as bookfile:
         reader = csv.reader(bookfile, delimiter = ',', quotechar = '"')
+        for row in reader:
+            book = row[0]
+            publishDate = row[1]
+            authorFirst = ""
+            authorLast = ""
+            birthYear = ""
+            deathYear = ""
+            deathDone = False
+            birthDone = False
+            lastDone = False
+            firstDone = False
+
+            i = len(row[2])-1
+            while i >= 0:
+                if deathDone == False:
+                    if row[2][i-1] == "-":
+                        deathYear = 0
+                        i = i - 1
+                    else:
+                        deathYear = "" + row[2][i-4] + row[2][i-3] + row[2][i-2] + row[2][i-1]
+                        i = i - 5
+                    deathDone = True
+                if birthDone == False and deathDone == True:
+                    birthYear = row[2][i-4] + row[2][i-3] + row[2][i-2] + row[2][i-1]
+                    i = i - 7
+                    birthDone = True
+
+                if lastDone == False and birthDone == True and deathDone == True:
+                    authorLast = authorLast + row[2][i]
+                    if row[2][i-1] == " ":
+                        i = i - 2
+                        lastDone = True
+
+                if firstDone == False and lastDone == True and birthDone == True and deathDone == True:
+                    authorFirst = authorFirst + row[2][i]
+                i = i - 1
+
+            authorFirst = switcharoo(authorFirst)
+            authorLast = switcharoo(authorLast)
+
+            newAuth = Author(authorFirst, authorLast, book, publishDate, birthYear, deathYear)
+
+            titleList.append(book)
+            authorList.append(authorLast + ", " + authorFirst)
+
         if action == "books":
-            for row in reader:
-                titleList.append(row[0])
-                print(row[0])
             #return list of books
             #add another if for sort-direction
-            print(titleList)
             return titleList
         elif action == "authors":
-            for row in reader:
-                authorList.append(row[2])
             #return list of authors
             #add another if for sort-direction
-            print(authorList)
             return authorList
         else:
             #return error message?
             pass
 
+def switcharoo(word):
+    correctWord = ""
+    for i in range(len(word)-1,-1,-1):
+        correctWord = correctWord + word[i]
+    return correctWord
 
 
-def sort(sortDirectionBool, myList):
+
+def sort(myList):
     #sorts list based on sortDirectionBool
     myList.sort()
+    print(myList)
+    myList.sort(reverse = True)
+    print(myList)
     return myList
     pass
 
@@ -68,7 +116,7 @@ def main():
     print(fileName)
     action = sys.argv[2]
     print(action)
-    if sys.argv.len() == 3:
+    if len(sys.argv) > 3:
         sortDirection = sys.argv[3]
         sortDirectionBool = False
         if (sortDirection.lower() == "reverse"):
@@ -77,5 +125,7 @@ def main():
         #sort forwards
     action = action.lower()
     actionList = readFile(fileName, action)
+    sortedList = sort(actionList)
+
 if __name__ == '__main__':
     main()
