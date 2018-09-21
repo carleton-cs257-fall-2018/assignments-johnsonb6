36e43582440822cd102eec6f0e3a61f95b85aa8b
Alexis Engel <alexisengel99@gmail.com>
2018-09-21 18:39:47 -0500

    finished writing the unit tests (we think)

--- testsV2.py
+++ testsV2.py
@@ -29,3 +29,30 @@ class BookTester(unittest.TestCase):
 
     def test_start_year(self):
         self.assertEqual(self.book_tester.books(start_year = 2000), ["All Clear", "Blackout"])
+
+    def test_end_year(self):
+        self.assertEqual(self.book_tester.books(end_year = 1820), ["Emma", "Pride and Prejudice", "Sense and Sensibility"])
+
+    def test_sortby_year(self):
+        self.assertEqual(self.book_tester.books(author_id = 1, sort_by = "year"), ["To Say Nothing of the Dog", "All Clear", "Blackout"])
+
+    def test_author_bookid(self):
+        self.assertEqual(self.book_tester.authors(book_id = 3), ["Connie Willis"])
+
+    def test_author_search_text(self):
+        self.assertEqual(self.book_tester.authors(search_text = "Wode"), ["Pelham Grenville Wodehouse"])
+
+    def test_author_start_year(self):
+        self.assertEqual(self.book_tester.authors(start_year = 2014), ["Gabriel Garcia Marquez", "Terry Pratchett"])
+
+    def test_author_end_year(self):
+        self.assertEqual(self.book_tester.authors(end_year = 1812), ["Charles Dickens"])
+
+    def test_sortby_birth_year(self):
+        self.assertEqual(self.book_tester.authors(search_text = "p"), ["Pelham Grenville Wodehouse", "Daphne DuMaurier", "Terry Pratchett"])
+
+    def test_right_books(self):
+        self.assertEqual(self.book_tester.books_for_author(1), ["All Clear", "Blackout", "To Say Nothing of the Dog"])
+
+    def test_right_authors(self):
+        self.assertEqual(self.book_tester.authors_for_book(7), ["Neil Gaiman", "Terry Pratchett"])
