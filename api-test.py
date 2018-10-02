688f5451fe02fbf561b02b4e6a4722e6e8c48bee
Brennan Johnson <brennanjohnson@eduroam-160-44.dyn.carleton.edu>
2018-10-01 19:29:21 -0500

    tried to figure out argparse. movies_in_theater() not working

--- api-test.py
+++ api-test.py
@@ -12,23 +12,6 @@ import json
 import urllib.request
 
 
-"""def translate_artist_id(artist_name):
-	artist_id = artist_name # need to change this somehow
-	'''
-	user will have inputted an artist. This function will translate the name of the artist
-	into the artist ID (to put into get_artist())
-	'''
-
-	return artist_id
-
-	'''
-	new_artist_ids = ""
-	for i_d in artist_ids:
-		new_id = translate_artist_id(i_d)
-		new_artist_ids += "," + new_id
-	'''
-	"""
-
 def get_top_movies():
 	# gives you a list of top movies
 	base_url = 'https://api.themoviedb.org/3/movie/popular?api_key=89036379e923b4f7b34eaa4b513982e5'
@@ -54,7 +37,7 @@ def movies_in_theater():
 
 
 def get_budget_for_movie(movie_id):
-	# returns a list of movies in theaters
+
 
 	base_url = 'https://api.themoviedb.org/3/movie/24?api_key=89036379e923b4f7b34eaa4b513982e5&language=en-US'
 	#url = base_url.format()
@@ -66,15 +49,26 @@ def get_budget_for_movie(movie_id):
 
 	return movie_name, movie_budget
 
+"""
+def main(args):
+    if args.action == 'movies_in_theater':
+        print(movies_in_theater())
+    elif args.action == 'get top movies':
+        print(get_top_movies())
+"""
 
 
 if __name__ == '__main__':
 
-	api_key = '89036379e923b4f7b34eaa4b513982e5'
-	#parser = argparse.ArgumentParser(description='Get artist info from the Spotify API')
-	i = int(sys.argv[1])
-	print(i)
-	movie_id = i
-	print(get_budget_for_movie(movie_id))
-
-	print(movies_in_theater())
+    api_key = '89036379e923b4f7b34eaa4b513982e5'
+    """
+    parser = argparse.ArgumentParser(description='Get movie info from the IMDB API')
+    parser.add_argument('action', metavar = 'action', help = "usage: 'movies in theater' or 'get top movies'", choices = ["movies_in_theater", "get top movies"])
+    args = parser.parse_args()
+    main(args)
+    """
+    i = int(sys.argv[1])
+    print(i)
+    movie_id = i
+    print(get_budget_for_movie(movie_id))
+    #print(movies_in_theater())
