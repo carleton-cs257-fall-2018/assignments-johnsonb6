cede4fd2cf520efdb81cef446b57760b3bcdc73f
Brennan Johnson <johnsonb6@carleton.edu>
2018-11-18 17:02:23 -0600

    documented and working

--- final/src/game_of_life/CellModel.java
+++ final/src/game_of_life/CellModel.java
@@ -7,6 +7,9 @@ part of MVC. CellModel is manipulated by Controller, and relays manipulations to
 
 
 public class CellModel {
+    /*
+    @author Brennan Johnson & Silas Monahan
+     */
     public enum CellValue {
         DEAD, ALIVE
     }
@@ -16,16 +19,28 @@ public class CellModel {
 
 
     public CellModel(int rowCount, int columnCount) {
+        /*
+        @param int num rows, int num columns
+        @return none
+         */
         assert rowCount > 0 && columnCount > 0;
         this.cells = new CellValue[rowCount][columnCount];
         this.startSimulation();
     }
 
     public void startSimulation() {
+        /*
+        @param none
+        @return none
+         */
         this.initializeSimulation();
     }
 
     private void initializeSimulation() {
+        /*
+        @param none
+        @return none
+         */
         int rowCount = this.cells.length;
         int columnCount = this.cells[0].length;
 
@@ -39,31 +54,56 @@ public class CellModel {
     }
 
     public CellValue getCellValue(int row, int column, CellValue[][] list) {
+        /*
+        @param int row, int column, CellValue[][] list of cells to be searched through
+        @return CellValue value of cell at row, column that was passed in through params
+         */
         assert row >= 0 && row < list.length && column >= 0 && column < list[0].length;
         return list[row][column];
     }
 
 
     public int getRowCount() {
+        /*
+        @param none
+        @return int number of rows in grid
+         */
         return this.cells.length;
     }
 
     public int getColumnCount() {
+        /*
+        @param none
+        @return int number of columns in grid
+         */
         assert this.cells.length > 0;
         return this.cells[0].length;
     }
 
 
     public void setCellDead(int row, int column, CellValue[][] list) {
+        /*
+        @param int row, int column, CellValue[][] list
+        @return none
+         */
         list[row][column] = CellValue.DEAD;
     }
 
     public void setCellAlive(int row, int column, CellValue[][] list) {
+        /*
+        @param int row, int column, CellValue[][] list
+        @return none
+         */
         list[row][column] = CellValue.ALIVE;
     }
 
     public int checkAdjacent(int row, int column, CellValue[][] list) {
-        // checks the status of the adjacent cells
+        /*
+        @param int row, int column, CellValue[][] list
+        @return int that represents num of adjacent cells that are alive
+        checks the status of the adjacent cells
+         */
+
         int rowCount = list.length;
         int columnCount = list[0].length;
 
--- final/src/game_of_life/Controller.java
+++ final/src/game_of_life/Controller.java
@@ -14,6 +14,9 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class Controller implements EventHandler<KeyEvent> {
+    /*
+    @author Brennan Johnson & Silas Monahan
+     */
     @FXML private CellView cellView;
     private CellModel cellModel;
     private Timer timer;
@@ -22,6 +25,10 @@ public class Controller implements EventHandler<KeyEvent> {
     }
 
     public void initialize() {
+        /*
+        @param none
+        @return none
+         */
         this.cellModel = new CellModel(this.cellView.getRowCount(), this.cellView.getColumnCount());
 
         this.cellView.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
@@ -34,17 +41,33 @@ public class Controller implements EventHandler<KeyEvent> {
     }
 
     public double getBoardWidth() {
+        /*
+        @param none
+        @return double that represents the width of the board (num columns)
+         */
         return CELL_WIDTH * this.cellView.getColumnCount();
     }
 
     public double getBoardHeight() {
+        /*
+        @param none
+        @return double that represents height of board (num rows)
+         */
         return CELL_WIDTH * this.cellView.getRowCount();
     }
 
     private void nextGeneration() {
+        /*
+        @param none
+        @return none
+         */
         this.cellView.nextGeneration(this.cellModel);
     }
     private void startTimer() {
+        /*
+        @param none
+        @return none
+         */
         this.timer = new java.util.Timer();
         TimerTask timerTask = new TimerTask() {
             public void run() {
@@ -56,11 +79,19 @@ public class Controller implements EventHandler<KeyEvent> {
         this.timer.schedule(timerTask, 0, frameTimeInMilliseconds);
     }
     private void pauseTimer() {
+        /*
+        @param none
+        @return none
+         */
         this.timer.cancel();
     }
 
     @Override
     public void handle(KeyEvent keyEvent) {
+        /*
+        @param KeyEvent from user, represents intended action to be applied
+        @return none
+         */
         KeyCode code = keyEvent.getCode();
 
         String s = code.getChar();
@@ -81,6 +112,10 @@ public class Controller implements EventHandler<KeyEvent> {
     }
 
     public void handleMouseEvent(MouseEvent mouseEvent) {
+        /*
+        @param MouseEvent that represents a click of the mouse on the game board
+        @return none
+         */
         int rowCount = cellModel.cells.length;
         int columnCount = cellModel.cells[0].length;
 
